95b1f31b634ecd07175b355691bc5da72dab6244
Neo319 <alexmnevins@gmail.com>
2024-02-04 11:43:03 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import test from '../src/test.js';
+import '../src/style.css';
+
+console.log('ready to roll!');
+test();
---
+++ src/test.js
@@ -0,0 +1,7 @@
+// test module
+
+const test = () => {
+    console.log('test successful!');
+}
+
+export default test;
