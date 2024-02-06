f6afc3cfdf1d9046749bca81fef0283aea69d909
Neo319 <alexmnevins@gmail.com>
2024-02-06 16:44:39 -0500

    add projectManager to commit

---
+++ src/app-logic/projectManager.js
@@ -0,0 +1,25 @@
+// module for adding, removing, and editing projects
+import ListItem from "../app-logic/itemMaker";
+
+const projectManager = (allProjects) => {
+
+        function createNewProject () { // create new project whose contents can be edited elsewhere, 
+            let myItem = new ListItem ('New To-do item', 'description', 'today', 1, false);
+            let myProj = ['New Project', myItem];
+            allProjects.push(myProj);
+        }
+
+        function addListItem (projectIndex) {
+            let myItem = new ListItem ('New To-do item', 'description', 'today', 1, false);
+            
+            let project = allProjects[projectIndex];
+            project.push(myItem);
+        }
+
+    return {
+        createNewProject,
+        addListItem,
+    }
+}
+
+export default projectManager;
